5d6ffc645164ce4182f8dfc58eeed8a62db944f3
Dave <dquigley89@gmail.com>
2023-11-01 11:03:28 -0400

    add geminus version to semver build identifier

--- tf2onnx/version.py
+++ tf2onnx/version.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-version = '1.15.1'
-git_version = 'dc6155b52a137d858456fcc6bc720c327eec5612'
+version = '1.15.1+geminus.1'
+git_version = '37820d9c15d9f90e8cd916c371f384175c2a713a'
